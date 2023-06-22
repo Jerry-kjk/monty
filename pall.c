8aa37b99c95d63169a77b133879cbc804b4d8033
Jerry-kjk <jerrykajik@gmail.com>
2023-06-22 07:38:17 -0700

    Update files

--- pall.c
+++ pall.c
@@ -4,13 +4,15 @@
  * f_pall - prints the stack
  * @head: stack head
  * @counter: unused parameter
+ * Return: no return.
  */
 
 void f_pall(stack_t **head, unsigned int counter)
 {
-	stack_t *h = *head;
+	stack_t *h;
 	(void)counter;
 
+	h = *head;
 	if (h == NULL)
 	{
 		return;
